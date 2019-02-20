6087674493a64673bd8714f69abc7b668787b819
JIElive <jielive@vip.qq.com>
2019-02-20 11:45:28 +0800

    更新2.4.3版

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 
-define('INITIAL_VERSION_NUMBER', '2.4.4');
+define('INITIAL_VERSION_NUMBER', '2.4.3');
 
 if (Helper::options()->GravatarUrl) define('__TYPECHO_GRAVATAR_PREFIX__', Helper::options()->GravatarUrl);
 
