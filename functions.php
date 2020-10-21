79a9c0421800c201884686e8e8595b00d5eb0cf7
JIElive <jielive@vip.qq.com>
2020-10-21 11:21:06 +0800

    更新2.5.2

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 error_reporting(0);
-define('INITIAL_VERSION_NUMBER', '2.5.1');
+define('INITIAL_VERSION_NUMBER', '2.5.2');
 if (Helper::options()->GravatarUrl) define('__TYPECHO_GRAVATAR_PREFIX__', Helper::options()->GravatarUrl);
 
 function themeConfig($form) {
