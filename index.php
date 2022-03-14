fb7bb0fc7ab3aedfd116839f7bd26fd770857230
Doubeecat <1359446223@qq.com>
2022-03-14 17:35:04 +0800

    add night mode & add code copy button

--- index.php
+++ index.php
@@ -18,30 +18,32 @@ $this->need('header.php');
 </article>
 <?php endif; ?>
 <?php while($this->next()): ?>
-<article class="post<?php if ($this->options->PjaxOption && $this->hidden): ?> protected<?php endif; ?>">
-<h2 class="post-title"><a href="<?php $this->permalink() ?>"><?php $this->title() ?></a></h2>
-<ul class="post-meta">
-<li><?php $this->date(); ?></li>
-<li><?php $this->category(',', false); ?></li>
-<li><?php $this->commentsNum('暂无评论', '%d 条评论'); ?></li>
-<li><?php Postviews($this); ?></li>
-</ul>
+    <article class="post<?php if ($this->options->PjaxOption && $this->hidden): ?> protected<?php endif; ?>">
+    <h2 class="post-title"><a href="<?php $this->permalink() ?>"><?php $this->title() ?></a></h2>
+    <ul class="post-meta">
+        <li><?php $this->date(); ?></li>
+        <li><?php $this->category(',', false); ?></li>
+        <li><?php $this->commentsNum('暂无评论', '%d 条评论'); ?></li>
+        <li><?php Postviews($this); ?></li>
+    </ul>
 <div class="post-content">
-<?php if ($this->options->PjaxOption && $this->hidden): ?>
-<form <?php if (!$this->options->AjaxLoad): ?>action="<?php echo Typecho_Widget::widget('Widget_Security')->getTokenUrl($this->permalink); ?>" <?php endif; ?>method="post">
-<p class="word">请输入密码访问</p>
-<p>
-<input type="password" class="text" name="protectPassword" />
-<input type="submit" class="submit" value="提交" />
-</p>
-</form>
-<?php else: ?>
-<?php if (postThumb($this)): ?>
-<p class="thumb"><?php echo postThumb($this); ?></p>
-<?php endif; ?>
-<p><?php $this->excerpt(200, ''); ?></p>
-<?php endif; ?>
-<p class="more"><a href="<?php $this->permalink() ?>" title="<?php $this->title() ?>">- 阅读全文 -</a></p>
+    <?php if ($this->options->PjaxOption && $this->hidden): ?>
+        <form <?php if (!$this->options->AjaxLoad): ?>action="<?php echo Typecho_Widget::widget('Widget_Security')->getTokenUrl($this->permalink); ?>" <?php endif; ?>method="post">
+            <p class="word">请输入密码访问</p>
+            <p>
+                <input type="password" class="text" name="protectPassword" />
+                <input type="submit" class="submit" value="提交" />
+            </p>
+        </form>
+    <?php else: ?>
+    <?php if (postThumb($this)): ?>
+        <p class="thumb"><?php echo postThumb($this); ?></p>
+    <?php endif; ?>
+    
+    <div class="post-content" itemprop="articleBody">
+        <?php $this->content('READ MORE'); ?>
+    </div>
+    <?php endif; ?>
 </div>
 </article>
 <?php endwhile; ?>
